f5e4295414ddb85049b65d46c24dbcb01aaf76f4
Riley Labrecque <git@rileylabrecque.com>
2021-04-08 16:06:51 -0700

    General cleanup of SimpleP4VSPackage.cs

--- Source/SimpleP4VSPackage.cs
+++ Source/SimpleP4VSPackage.cs
@@ -12,19 +12,14 @@ namespace SimpleP4VS
 
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
-    //[ProvideOptionPage(typeof(Options), "Web", Vsix.Name, 101, 102, true, new string[0], ProvidesLocalizedCategoryName = false)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(PackageGuids.guidSimpleP4VSPackageString)]
     [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExistsAndFullyLoaded_string, PackageAutoLoadFlags.BackgroundLoad)]
     public sealed class SimpleP4VSPackage : AsyncPackage
     {
-        //public static Options Options { get; private set; }
-
-        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<Microsoft.VisualStudio.Shell.ServiceProgressData> progress)
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
-            //Options = (Options)GetDialogPage(typeof(Options));
             OleMenuCommandService commandService = await GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
-
             if (commandService != null)
             {
                 await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
